639ad1a96d6c9a3435b693d3f38b512304f112f3
RishabhRawat121 <rishabhrawat150leetcode@gmail.com>
2025-09-21 19:55:17 +0530

    Update settings.py

--- socialconnect/settings.py
+++ socialconnect/settings.py
@@ -36,17 +36,16 @@ AUTHENTICATION_BACKENDS = [
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",  # must be first
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
 ]
+
 
 ROOT_URLCONF = "socialconnect.urls"
 
