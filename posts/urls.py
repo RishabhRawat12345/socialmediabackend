41e9b05ce13cae71920439e653234fe236c1ae64
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2025-09-23 21:18:34 +0530

    Update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostListCreateView, PostDetailView
+from .views import PostListCreateView, PostDetailView,MyPostListView
 
 urlpatterns = [
     path('', PostListCreateView.as_view(), name='post-list-create'),
     path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('myposts/', MyPostListView.as_view(), name='my-posts'),
 ]
