ac8eb219d8b191c7af7de3d567343c4e73dcb336
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2025-09-24 06:38:50 +0530

    Update urls.py

--- engagement/urls.py
+++ engagement/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import LikePostView, LikeStatusView, CommentListCreateView, CommentDeleteView
+from .views import (
+    PostListView, PostDetailView,
+    LikePostView, CommentListCreateView
+)
 
 urlpatterns = [
+    path('posts/', PostListView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+
     path('posts/<int:post_id>/like/', LikePostView.as_view(), name='like-post'),
-    path('posts/<int:post_id>/like/status/', LikeStatusView.as_view(), name='like-status'),
     path('posts/<int:post_id>/comments/', CommentListCreateView.as_view(), name='post-comments'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
 ]
