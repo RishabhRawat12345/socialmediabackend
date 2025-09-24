890057910116f6a7bebf9b3309aff108e4af8338
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2025-09-24 09:28:10 +0530

    Update signals.py

--- notifications/signals.py
+++ notifications/signals.py
@@ -1,39 +1,42 @@
-# notifications/signals.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from followers.models import Follow
-from posts.models import Post
-from engagement.models import Like, Comment
-from .models import Notification
+from followers.models import Follow  # your follow model
+from posts.models import Post, Comment  # your post/comment models
+from notifications.models import Notification
 
+# Follow notification
 @receiver(post_save, sender=Follow)
 def create_follow_notification(sender, instance, created, **kwargs):
-    if created:
+    if created and instance.follower != instance.following:
         Notification.objects.create(
-            recipient=instance.following,
             sender=instance.follower,
+            recipient=instance.following,
             notification_type='follow',
-            message=f'{instance.follower.username} started following you'
+            message=f"{instance.follower.username} started following you"
         )
 
-@receiver(post_save, sender=Like)
+# Like notification
+@receiver(post_save, sender=Post.likes.through)  # if using ManyToManyField for likes
 def create_like_notification(sender, instance, created, **kwargs):
-    if created:
+    post = instance.post
+    user = instance.user
+    if created and post.author != user:
         Notification.objects.create(
-            recipient=instance.post.author,
-            sender=instance.user,
+            sender=user,
+            recipient=post.author,
             notification_type='like',
-            post=instance.post,
-            message=f'{instance.user.username} liked your post'
+            post=post,
+            message=f"{user.username} liked your post"
         )
 
+# Comment notification
 @receiver(post_save, sender=Comment)
 def create_comment_notification(sender, instance, created, **kwargs):
-    if created:
+    if created and instance.author != instance.post.author:
         Notification.objects.create(
+            sender=instance.author,
             recipient=instance.post.author,
-            sender=instance.user,
             notification_type='comment',
             post=instance.post,
-            message=f'{instance.user.username} commented on your post'
+            message=f"{instance.author.username} commented on your post"
         )
