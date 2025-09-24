7eaf93a627be5bcc2c75e9f9dcf82bc5d13aff2e
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2025-09-24 09:49:10 +0530

    Update signals.py

--- notifications/signals.py
+++ notifications/signals.py
@@ -1,48 +1,50 @@
-from django.db.models.signals import post_save, m2m_changed
+from django.db.models.signals import post_save
 from django.dispatch import receiver
-from followers.models import Follow  # Your follow model
-from posts.models import Post, Comment
+from posts.models import Post
+from users.models import CustomUser
 from .models import Notification
 
-# ---------------------------
-# Follow notifications
-# ---------------------------
-@receiver(post_save, sender=Follow)
+# Only import Comment if it exists
+try:
+    from posts.models import Comment
+except ImportError:
+    Comment = None
+
+# Follow notification
+@receiver(post_save, sender=CustomUser)
 def create_follow_notification(sender, instance, created, **kwargs):
     if created:
+        # Example: send a welcome notification to new users
         Notification.objects.create(
-            recipient=instance.following,
-            sender=instance.follower,
+            recipient=instance,
+            sender=instance,
             notification_type='follow',
-            message=f"{instance.follower.username} started following you."
+            message=f"Welcome {instance.username}!",
         )
 
-# ---------------------------
-# Comment notifications
-# ---------------------------
-@receiver(post_save, sender=Comment)
-def create_comment_notification(sender, instance, created, **kwargs):
-    if created and instance.author != instance.post.author:
-        Notification.objects.create(
-            recipient=instance.post.author,
-            sender=instance.author,
-            post=instance.post,
-            notification_type='comment',
-            message=f"{instance.author.username} commented on your post."
-        )
-
-# ---------------------------
-# Like notifications
-# ---------------------------
-@receiver(m2m_changed, sender=Post.likes.through)
-def create_like_notification(sender, instance, action, pk_set, **kwargs):
-    if action == "post_add":
-        for user_id in pk_set:
-            if instance.author.id != user_id:
+# Like notification
+@receiver(post_save, sender=Post)
+def create_like_notification(sender, instance, created, **kwargs):
+    if not created and instance.likes.exists():  # Assuming you have a likes field
+        for user in instance.likes.all():
+            if user != instance.author:
                 Notification.objects.create(
                     recipient=instance.author,
-                    sender_id=user_id,
+                    sender=user,
                     post=instance,
                     notification_type='like',
-                    message=f"{instance.likes.get(id=user_id).username} liked your post."
+                    message=f"{user.username} liked your post."
                 )
+
+# Comment notification (optional if Comment exists)
+if Comment:
+    @receiver(post_save, sender=Comment)
+    def create_comment_notification(sender, instance, created, **kwargs):
+        if created and instance.author != instance.post.author:
+            Notification.objects.create(
+                recipient=instance.post.author,
+                sender=instance.author,
+                post=instance.post,
+                notification_type='comment',
+                message=f"{instance.author.username} commented on your post."
+            )
