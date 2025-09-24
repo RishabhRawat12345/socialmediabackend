b045d45b0184703eb36021d83ab2a6b3a81e416d
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2025-09-24 10:52:41 +0530

    Update utils.py

--- notifications/utils.py
+++ notifications/utils.py
@@ -1,1 +1,13 @@
+from .models import Notification
+
+def create_notification(sender, recipient, notification_type, post=None, message=""):
+    if sender == recipient:
+        return  # Don't notify yourself
+    Notification.objects.create(
+        sender=sender,
+        recipient=recipient,
+        notification_type=notification_type,
+        post=post,
+        message=message
+    )
 
