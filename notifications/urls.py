0c91fbc73824b5a2528d922c47e04db14a81f733
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2025-09-24 14:08:54 +0530

    Update urls.py

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import NotificationListView, mark_notification_read, mark_all_notifications_read
+from .views import NotificationListView, mark_notification_read, mark_all_notifications_read, create_notification
 
 urlpatterns = [
     path('', NotificationListView.as_view(), name='notifications-list'),
     path('<int:notification_id>/read/', mark_notification_read, name='notification-read'),
     path('mark-all-read/', mark_all_notifications_read, name='notifications-mark-all-read'),
+    path('create/', create_notification, name='notification-create'),
 ]
