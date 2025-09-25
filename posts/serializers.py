90fbc79f0b3fc4e7e163db0e67f7953f905229af
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2025-09-25 13:40:21 +0530

    Update serializers.py

--- posts/serializers.py
+++ posts/serializers.py
@@ -66,12 +66,15 @@ class PostSerializer(serializers.ModelSerializer):
         
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    author_username = serializers.CharField(source='author.username', read_only=True)
+# serializers.py
+from rest_framework import serializers
+from .models import Comment
 
+class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['id', 'post', 'author', 'author_username', 'content', 'created_at']
+        fields = ['id', 'author', 'post', 'content', 'created_at']
+        read_only_fields = ['id', 'author', 'post', 'created_at']
 
 
 
