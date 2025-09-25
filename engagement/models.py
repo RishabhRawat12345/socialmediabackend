487fac94762ac561f6f83d75a4476550a45350e9
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2025-09-25 11:10:15 +0530

    Update models.py

--- engagement/models.py
+++ engagement/models.py
@@ -2,41 +2,31 @@ from django.db import models
 from django.conf import settings
 from posts.models import Post
 
-
+# ---------------- Engagement Like ----------------
 class Like(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name="likes_given"  # all likes by a user
+        related_name='user_engagement_likes'
     )
     post = models.ForeignKey(
         Post,
         on_delete=models.CASCADE,
-        related_name="likes"  # all likes on a post
+        related_name='engagement_likes'
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        unique_together = ('user', 'post')
-
-    def __str__(self):
-        return f"{self.user.username} likes Post {self.post.id}"
-
-
+# ---------------- Engagement Comment ----------------
 class Comment(models.Model):
-    content = models.TextField(max_length=200)
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='engagement_comments'
+    )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name="comments_given"  # all comments by a user
+        related_name='user_engagement_comments'
     )
-    post = models.ForeignKey(
-        Post,
-        on_delete=models.CASCADE,
-        related_name="comments"  # all comments on a post
-    )
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return f"Comment by {self.author.username} on Post {self.post.id}"
