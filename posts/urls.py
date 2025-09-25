54f3f2898d4ee91f3eadd2cd39e6522b16f05687
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2025-09-25 09:43:14 +0530

    Update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import PostListCreateView, PostDetailView,MyPostListView, LikePostView, CommentCreateView
+from .views import (
+    PostListCreateView, PostDetailView, MyPostListView,
+    LikePostView, CommentCreateView,
+    NotificationListView, MarkNotificationReadView
+)
 
 urlpatterns = [
     path('', PostListCreateView.as_view(), name='post-list-create'),
@@ -7,4 +11,8 @@ urlpatterns = [
     path('myposts/', MyPostListView.as_view(), name='my-posts'),
     path('<int:post_id>/like/', LikePostView.as_view(), name='post-like'),
     path('comment/', CommentCreateView.as_view(), name='post-comment'),
+
+    # Notifications
+    path('notifications/', NotificationListView.as_view(), name='notifications'),
+    path('notifications/<int:notification_id>/read/', MarkNotificationReadView.as_view(), name='mark-notification-read'),
 ]
