ed8642c95958b8cb8f2ad129d241c9d09961d7d6
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2025-09-26 10:42:55 +0530

    Update models.py

--- followers/models.py
+++ followers/models.py
@@ -1,21 +1,15 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Follow(models.Model):
-    follower = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='following_set'
-    )
-    following = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='followers_set'
-    )
+    follower = models.ForeignKey(User, related_name="following_set", on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name="followers_set", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         unique_together = ('follower', 'following')
 
     def __str__(self):
-        return f"{self.follower.username} -> {self.following.username}"
+        return f"{self.follower.username} → {self.following.username}"
