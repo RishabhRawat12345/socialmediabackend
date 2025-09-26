0dc10ee2708c2fdc4c1a9ebeb625c2403f685b02
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2025-09-26 11:39:30 +0530

    Update urls.py

--- socialconnect/urls.py
+++ socialconnect/urls.py
@@ -4,7 +4,7 @@ urlpatterns = [
     path('api/auth/', include('users.urls')),
     path('api/users/', include('profiles.urls')),
     path('api/posts/', include('posts.urls')),
-    path('api/users/', include('followers.urls')),
+    path('api/followers/', include('followers.urls')),
     path('api/', include('engagement.urls')),
     path('api/', include('feed.urls')),
     path('api/', include('notifications.urls')),  # <-- Add this line
