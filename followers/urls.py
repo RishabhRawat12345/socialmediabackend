ef971c05ac0dfad6430224e6a0eaa92670fbfbcf
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2025-09-26 10:43:51 +0530

    Update urls.py

--- followers/urls.py
+++ followers/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import FollowUserView, FollowersListView, FollowingListView
+from .views import FollowUserView, FollowersListView, FollowingListView, FollowStatsView
 
 urlpatterns = [
     path('<int:user_id>/follow/', FollowUserView.as_view(), name='follow-user'),
     path('<int:user_id>/followers/', FollowersListView.as_view(), name='user-followers'),
     path('<int:user_id>/following/', FollowingListView.as_view(), name='user-following'),
+    path('<int:user_id>/stats/', FollowStatsView.as_view(), name='follow-stats'),
 ]
