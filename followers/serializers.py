b020bfc573b1092b4c3a1250c5c01cf250614ef9
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2025-09-26 10:43:12 +0530

    Update serializers.py

--- followers/serializers.py
+++ followers/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from .models import Follow
 
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
 class FollowSerializer(serializers.ModelSerializer):
-    follower = serializers.CharField(source='follower.username', read_only=True)
-    following = serializers.CharField(source='following.username', read_only=True)
+    follower = UserSerializer(read_only=True)
+    following = UserSerializer(read_only=True)
 
     class Meta:
         model = Follow
